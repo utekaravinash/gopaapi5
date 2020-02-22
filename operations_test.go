5cd763e192531735fd94a397d9e274bea5c5ecc3
Avinash U <utekaravinash@gmail.com>
2020-02-22 16:38:05 +0530

    bug fixes and new tests

--- operations_test.go
+++ operations_test.go
@@ -116,6 +116,106 @@ func TestGetItems(t *testing.T) {
 	}
 }
 
+func TestGetVariations(t *testing.T) {
+	params := api.GetVariationsParams{}
+
+	client, err := NewClient("accessKey", "secretKey", "associateTag", api.UnitedStates)
+	if err != nil {
+		t.Fatalf("Error getting client: %s", err)
+	}
+	client.testing = true
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		file, err := os.Open("_response/get_variations_success.json")
+		if err != nil {
+			t.Errorf("Cannot open _response/get_variations_success.json")
+		}
+
+		jo, err := ioutil.ReadAll(file)
+		if err != nil {
+			t.Errorf("Cannot read _response/get_variations_success.json")
+		}
+
+		w.Write(jo)
+	})
+
+	httpClient, teardown := testingHTTPClient(h)
+	defer teardown()
+
+	client.httpClient = httpClient
+
+	response, err := client.GetVariations(&params)
+	if err != nil {
+		t.Fatalf("Error in client.GetVariations: %s", err)
+	}
+
+	tests := []struct {
+		name     string
+		expected interface{}
+		actual   interface{}
+	}{
+		{"Count Items", 9, len(response.VariationsResult.Items)},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.expected != test.actual {
+				t.Errorf("Expected: %v, Actual: %v", test.expected, test.actual)
+			}
+		})
+	}
+}
+
+func TestSearchItems(t *testing.T) {
+	params := api.SearchItemsParams{}
+
+	client, err := NewClient("accessKey", "secretKey", "associateTag", api.UnitedStates)
+	if err != nil {
+		t.Fatalf("Error getting client: %s", err)
+	}
+	client.testing = true
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		file, err := os.Open("_response/search_items_success.json")
+		if err != nil {
+			t.Errorf("Cannot open _response/search_items_success.json")
+		}
+
+		jo, err := ioutil.ReadAll(file)
+		if err != nil {
+			t.Errorf("Cannot read _response/search_items_success.json")
+		}
+
+		w.Write(jo)
+	})
+
+	httpClient, teardown := testingHTTPClient(h)
+	defer teardown()
+
+	client.httpClient = httpClient
+
+	response, err := client.SearchItems(&params)
+	if err != nil {
+		t.Fatalf("Error in client.SearchItems: %s", err)
+	}
+
+	tests := []struct {
+		name     string
+		expected interface{}
+		actual   interface{}
+	}{
+		{"Count Items", 3, len(response.SearchResult.Items)},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.expected != test.actual {
+				t.Errorf("Expected: %v, Actual: %v", test.expected, test.actual)
+			}
+		})
+	}
+}
+
 func testingHTTPClient(handler http.Handler) (*http.Client, func()) {
 	s := httptest.NewServer(handler)
 
