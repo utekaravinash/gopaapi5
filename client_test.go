660db260859f5e8504986322772d197ddc67c96d
Avinash U <utekaravinash@gmail.com>
2020-03-02 12:35:53 +0530

    updated responses with all resources

--- client_test.go
+++ client_test.go
@@ -17,6 +17,23 @@ func TestNewClient(t *testing.T) {
 	_, err3 := NewClient("accessKey", "secretKey", "", api.UnitedStates)
 	_, err4 := NewClient("accessKey", "secretKey", "associateTag", api.Locale("Fake Locale"))
 
+	err5 := client.executeOperation(api.GetItems, api.GetItemsParams{}, nil)
+	err6 := client.executeOperation(api.GetItems, nil, nil)
+	err7 := client.executeOperation(api.GetItems, api.GetItemsParams{Resources: []api.Resource{api.VariationSummaryVariationDimension}}, nil)
+
+	req := &request{
+		operation: api.GetItems,
+		payload:   map[string]interface{}{},
+		path:      "paapi5/getitems",
+		client:    client,
+		dateTime:  testRequestTime,
+	}
+
+	err = req.build()
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
 	tests := []struct {
 		name     string
 		expected interface{}
@@ -29,6 +46,9 @@ func TestNewClient(t *testing.T) {
 		{"Empty SecretKey", "Empty secret key", err2.Error()},
 		{"Empty AssociateTag", "Empty associate tag", err3.Error()},
 		{"Invalid Locale", "Invalid locale", err4.Error()},
+		{"NoItemIds", "One or more item ids required", err5.Error()},
+		{"NilParameters", "Nil parameters", err6.Error()},
+		{"InvalidResource", `Invalid resource "VariationSummary.VariationDimension" for operation "GetItems"`, err7.Error()},
 	}
 
 	for _, test := range tests {
